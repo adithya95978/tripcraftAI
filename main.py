422f95f8daba81b1f000db00f91a1406bbff5528
adithya95978 <adithyachinta2005@gmail.com>
2025-10-18 10:43:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from agent.agentic_wf import GraphBuilder
+from utils.save_to_doc import save_document
+from starlette.responses import JSONResponse
+import os
+import datetime
+from dotenv import load_dotenv
+from pydantic import BaseModel
+load_dotenv()
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # set specific origins in prod
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+class QueryRequest(BaseModel):
+    question: str
+
+@app.post("/query")
+async def query_travel_agent(query:QueryRequest):
+    try:
+        print(query)
+        graph = GraphBuilder(model_provider="groq")
+        react_app=graph()
+        messages={"messages": [query.question]}
+        config = {"configurable": {"thread_id": 1}}
+        output = react_app.invoke(messages,config=config)
+        if isinstance(output, dict) and "messages" in output:
+            final_output = output["messages"][-1].content  # Last AI response
+        else:
+            final_output = str(output)
+        
+        return {"answer": final_output}
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": str(e)})
